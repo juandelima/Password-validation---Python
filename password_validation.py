7917d9d7eb9eb19ea1be61d9c5d09f125f85ac6b
juanvaleriand <juanvaleriand@gmail.com>
2018-12-06 20:03:41 +0700

    Password Validation

--- password_validation.py
+++ password_validation.py
@@ -1,42 +1,80 @@
 #CREATED BY JUAN VALERIAN DELIMA
-def cek_simbol(string):
-   check = False
-   for s in string:
-      if s in "~!@#$%^&*()_+{}|':;<>?":
-         check = True
-   return check
+#the algorithm is made by juan
 
-def cek_later(string):
-   check = False
-   for s in string:
-      if s in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ":
-         check = True
-   return check
+def create_lists(kata):
+   new_arr = []
+   for kata1 in kata:
+      for huruf in kata1:
+         new_arr.append(huruf)
+   return new_arr
 
-def cek_number(string):
-   check = False
-   for s in string:
-      if s in "0123456789":
-         check = True
-   return check
+def sorting_kata(password): 
+   arr = create_lists(password)
+   for index in range(1, len(password)):
+      current_word = arr[index]
+      index_kata = index
+      while index_kata > 0 and arr[index_kata - 1] > current_word:
+         arr[index_kata] = arr[index_kata - 1]
+         index_kata -= 1
+      arr[index_kata] = current_word
+   return arr
+      
+def pencarian_huruf(password, cari): #teknik rekursif
+   arr = sorting_kata(password)
+   if len(arr) == 0:
+      return False
+   else:
+      split_word = len(arr) // 2
+      if arr[split_word ] == cari:
+         return True
+      else:
+         if cari < arr[split_word]:
+            return pencarian_huruf(arr[:split_word], cari)
+         else:
+            return pencarian_huruf(arr[split_word + 1:], cari)
 
-def main_program():
-   repeat = True
-   while repeat:
+def cek_simbol(password):
+   sym = "~!@#$%^&*()_{}'|:;"
+   lists_sym = create_lists(sym)
+   for simbol in range(len(lists_sym)):
+      if pencarian_huruf(password, lists_sym[simbol]) == True:
+         return True
+
+def cek_kata(password):
+   words = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
+   lists_wrd = create_lists(words)
+   for word in range(len(lists_wrd)):
+      if pencarian_huruf(password, lists_wrd[word]) == True:
+         return True
+         
+
+def cek_angka(password):
+   numbers = "0123456789"
+   list_num = create_lists(numbers)
+   for angka in range(len(list_num)):
+      if pencarian_huruf(password, list_num[angka]) == True:
+         return True
+
+def program_utama():
+   ulang = False
+   while not ulang:
       password = input("Masukkan Password : ")
       if len(password) < 6:
-         print("Password tidak boleh kurang dari 6 karakter")
-         repeat =True
+         print("Panjang password harus lebih dari 6!")
+         ulang = False
       else:
-         if (cek_later(password) or cek_simbol(password)) and cek_number(password):
-            print("Password Valid")
-            coba = input("Ingin Coba Lagi ? (Y/N) : ")
+         if (cek_kata(password) or cek_simbol(password)) and cek_angka(password):
+            print("Password Valid!")
+            coba = input("Ulangi Program ? Y/N : ")
             if coba == 'Y' or coba == 'y':
-               repeat = True
-            elif coba == 'N' or coba == 'n':
-               repeat = False
+               ulang = False
+            else:
+               if coba == 'N' or coba == 'n':
+                  ulang = True
          else:
-            print("Password Tidak Valid")
-            repeat =True
+            print("Password Tidak Valid!")
 
-main_program()
+
+program_utama()
+
+               
